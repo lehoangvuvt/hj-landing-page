1d23ac1e2c477586c83e495a6475013d9cb41f9b
VuLH <lehoangvu@bacoor.co>
2024-06-08 03:16:00 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@
 import Image from "next/image";
 import logo from "/public/images/logo.png";
 import homeBanner from "/public/images/home-banner.png";
+import "./home.css";
 
 export default function Home() {
-  const fontHighlightClass = "text-[#EC407A]";
+  const primaryColor = "#EC407A";
+  const fontHighlightClass = `text-[${primaryColor}]`;
 
   return (
     <div className="w-screen flex flex-col px-[60px] items-center">
       <div className="w-[80%] flex flex-row items-center justify-center">
-        <div className="w-[20%] aspect-video relative">
+        <div className="w-[23%] aspect-video relative -mt-[30px]">
           <Image
             src={logo.src}
             fill
@@ -35,16 +37,24 @@ export default function Home() {
             <span className={fontHighlightClass}>moments.</span> <br />
           </div>
           <div className="w-full flex flex-row mt-[20px] gap-[15px] items-center">
-            <button className="bg-[#131418] text-[0.9rem] font-medium rounded-[2rem] text-[white] px-[30px] py-[15px]">
-              Get Started
+            <button
+              onClick={() => {
+                window.open("https://podcast.healing-journey.asia");
+              }}
+              className={`bg-[#131418] text-[0.9rem] font-medium rounded-[2rem] text-[white] px-[30px] py-[15px] hover:bg-[${primaryColor}] transition-all`}
+            >
+              Listen Here
             </button>
-            <button className="text-[1rem] font-bold rounded-[2rem] text-[rgba(0,0,0,0.8)] px-[30px] py-[15px] hover:underline">
+            <button
+              className={`text-[1rem] font-bold rounded-[2rem] text-[rgba(0,0,0,0.8)] px-[30px] py-[15px] hover:text-[${primaryColor}] transition-all`}
+            >
               Learn more
             </button>
           </div>
         </div>
         <div className="flex-1 flex h-[75vh] relative">
           <Image
+            className="home-banner tilt"
             src={homeBanner.src}
             fill
             objectFit="contain"
